e7b11b57abd47f6f624ac2bedc711364ce090853
zax <zaxhutchinson@gmail.com>
2017-04-04 19:00:42 -0400

    App map work

--- SavingMainiacsApp/app/src/main/java/com/mainiacs/saving/savingmainiacsapp/MainActivity.java
+++ SavingMainiacsApp/app/src/main/java/com/mainiacs/saving/savingmainiacsapp/MainActivity.java
@@ -1,13 +1,32 @@
 package com.mainiacs.saving.savingmainiacsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGoToMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGoToMap = (Button)findViewById(R.id.button2);
+        btnGoToMap.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                StartMapActivity(view);
+            }
+        });
+
+    }
+
+    public void StartMapActivity(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
