f824cd964058dcabc7ac066700d9852b17164e07
Kent Seneres <kent.seneres@gmail.com>
2017-04-29 16:21:07 -0400

    Set up user profile page

--- SavingMainiacsApp/app/src/main/java/com/mainiacs/saving/savingmainiacsapp/MainActivity.java
+++ SavingMainiacsApp/app/src/main/java/com/mainiacs/saving/savingmainiacsapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.mainiacs.saving.savingmainiacsapp;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -21,14 +23,25 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private static String USER_PICTURE_URL = "https://abnet.ddns.net/mucoftware/remote/get_user_picture.php?userid=";
+    private static final int MAX_STEPS = 10000;
 
     DataManager dm;
     UserProfile user;
@@ -61,17 +74,42 @@ public class MainActivity extends AppCompatActivity
         dm = getIntent().getParcelableExtra("DataManager");
         user = dm.userProfile;
 
-        GetUserPicture();
         PopulateUserData();
 
     }
 
     void PopulateUserData() {
-        TextView daySteps = (TextView)findViewById(R.id.userDaySteps);
-        TextView coins = (TextView)findViewById(R.id.userCoins);
+        TextView daySteps = (TextView) findViewById(R.id.userDaySteps);
+        TextView coins = (TextView) findViewById(R.id.userCoins);
+        TextView level = (TextView) findViewById(R.id.userLevel);
 
         daySteps.setText(Integer.toString(user.DaySteps()));
         coins.setText(Integer.toString(user.Coins()));
+        level.setText("Level ?");
+
+        GetUserPicture();
+
+        int steps = user.DaySteps();
+        int zero = MAX_STEPS - steps;
+        PieChart stepChart = (PieChart) findViewById(R.id.steps_chart);
+        stepChart.setOnTouchListener(null);
+        stepChart.setDescription(null);
+        stepChart.setDrawEntryLabels(false);
+        stepChart.getLegend().setEnabled(false);
+
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry((float) steps, 0));
+        entries.add(new PieEntry((float) zero, 1));
+
+        PieDataSet dataSet = new PieDataSet(entries, "Steps");
+        int[] colors = {ContextCompat.getColor(this, R.color.bsbBlue), ContextCompat.getColor(this, R.color.lightGrey)};
+        dataSet.setColors(colors);
+        dataSet.setDrawValues(false);
+
+        PieData data = new PieData(dataSet);
+        stepChart.setData(data);
+        stepChart.invalidate();
+
     }
 
     void GetUserPicture() {
@@ -85,15 +123,14 @@ public class MainActivity extends AppCompatActivity
             public void onResponse(JSONObject jsonObject) {
                 //System.out.println(jsonObject.toString());
                 try {
-                    if(jsonObject.getInt("success")==1) {
+                    if (jsonObject.getInt("success") == 1) {
 
                         String base64encodedPic = jsonObject.getString("data");
                         byte[] picbyes = Base64.decode(base64encodedPic, Base64.DEFAULT);
                         user.Picture = BitmapFactory.decodeByteArray(picbyes, 0, picbyes.length);
-                        ImageView pic = (ImageView)findViewById(R.id.userpic);
+                        ImageView pic = (ImageView) findViewById(R.id.userpic);
                         pic.setImageBitmap(user.Picture);
-                    }
-                    else {
+                    } else {
 
                     }
                 } catch (JSONException e) {
